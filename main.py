3aa2219c23ef882527795bc3919244ad99553dfa
Mathews <mathewsrodrigues9@gmail.com>
2023-05-17 18:12:39 -0300

    criando funcao para salvar o arquivo em xlsx

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from PyPDF2 import PdfReader
+import pandas as pd
 
 from time import sleep
 
@@ -27,4 +29,18 @@ def get_oficial_diare():
     sleep(5)
 
 
-get_oficial_diare()
+def read_pdf():
+    pdf = open("./oficial_diare/rio_de_janeiro_2023-05-17_completo.pdf", "rb")
+
+    teste = PdfReader(pdf)
+    pagina = teste.pages[0]
+    content = pagina.extract_text()
+
+    tabela = pd.read_excel("excel/testando.xlsx")
+    tabela.to_excel(
+        "excel/Diário_Oficial_Cidade_RJ_20230517.xlsx", index=False
+    )
+
+
+# get_oficial_diare()
+read_pdf()
