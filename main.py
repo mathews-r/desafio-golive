5ec12234699717b1949250913248eb932afd27c5
Mathews <mathewsrodrigues9@gmail.com>
2023-05-18 18:50:11 -0300

    criando funcao para pegar os dados topicos do pdf e salvar no excel

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import os
+import pandas as pd
+from time import sleep
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from PyPDF2 import PdfReader
 
-# import pandas as pd
 
-from time import sleep
-
-# AJUSTAR A DATA AUTOMATICA DEPOIS
-PATH_FILE = "./oficial_diare/rio_de_janeiro_2023-05-17_completo.pdf"
+date_excel = f"{datetime.today():%Y%m%d}"
+date_pdf = f"{datetime.today():%Y-%m-%d}"
+PATH_FILE = f"./oficial_diare/rio_de_janeiro_{date_pdf}_completo.pdf"
+URL = "https://doweb.rio.rj.gov.br/"
 
 
 def get_oficial_diare():
-    URL = "https://doweb.rio.rj.gov.br/"
-
     # Diretório para download do arquivo
     download_dir = (
         f"{os.path.dirname(os.path.realpath(__file__))}/oficial_diare"
@@ -38,30 +38,59 @@ def get_oficial_diare():
 
     browser.find_element(By.ID, "imagemCapa").click()
 
+    # Tempo que o browser fica aberto (em segundos)
     sleep(5)
 
 
-def read_pdf(path_file):
-    # get_oficial_diare()
+def read_pdf(path_file) -> None:
+    get_oficial_diare()
 
+    # Faz a leitura do PDF
     pdf = PdfReader(path_file)
+    number_of_pages = len(pdf.pages)
 
-    lista = []
+    print(pdf.metadata.creation_date)
 
-    for index, pdf_pag in enumerate(pdf.pages):
-        content = {index + 1: pdf_pag.extract_text()}
-        lista.append(content)
-    with open("./excel/page_xablau.txt", "a", encoding="utf-8") as txt_file:
-        txt_file.write(str(lista))
+    topics_list = []
+
+    # Percorre as páginas, extrai os textos e transforma em um array
+    for page in range(number_of_pages):
+        content = pdf.pages[page].extract_text()
+        data = content.split("\n")
+
+        # Percorre a nova lista, verifica se esta em Uppercase e adiciona
+        #  o novo dict no array topic_list
+        for topics in data:
+            if topics.isupper():
+                topic_dict = {f"{page + 1}": topics}
+                topics_list.append(topic_dict)
+
+    # Filtra o resultado e pega somente os valores que foram duplicados
+    filter_repeated(topics_list)
 
 
 # 2 - {'atos': 1, 'do': 1, 'prefeito': 1}
 
-# AJUSTAR O NOME DO ARQUIVO AUTOMATICO
-#     tabela = pd.read_excel("excel/Diário_Oficial_Cidade_RJ.xlsx")
-#     tabela.to_excel(
-#         "excel/Diário_Oficial_Cidade_RJ_20230517.xlsx", index=False
-#     )
+
+def filter_repeated(list_dicts) -> None:
+    ocurrences = {}
+    repeated_values = []
+
+    for dicts in list_dicts:
+        for value in dicts.values():
+            if value in ocurrences:
+                if ocurrences[value] == 1:
+                    repeated_values.append({f"{list(dicts.keys())[0]}": value})
+                ocurrences[value] += 1
+            else:
+                ocurrences[value] = 1
+    with open("./excel/pdf_text.txt", "a", encoding="utf-8") as txt_file:
+        txt_file.write(str(repeated_values))
+    tabela = pd.read_excel(f"excel/Diário_Oficial_Cidade_RJ_{date_excel}.xlsx")
+    tabela.to_excel(
+        f"excel/Diário_Oficial_Cidade_RJ_{date_excel}.xlsx", index=False
+    )
 
 
+# Exemplo de uso
 read_pdf(PATH_FILE)
