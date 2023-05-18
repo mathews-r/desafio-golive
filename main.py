c814065bb87f0eab5ed3ed476599bf7e8305d4d4
Mathews <mathewsrodrigues9@gmail.com>
2023-05-17 22:09:26 -0300

    fazendo alguns testes na funcao que extrai dados do pdf

--- main.py
+++ main.py
@@ -3,18 +3,24 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from PyPDF2 import PdfReader
-import tabula
 
 # import pandas as pd
 
 from time import sleep
 
+# AJUSTAR A DATA AUTOMATICA DEPOIS
+PATH_FILE = "./oficial_diare/rio_de_janeiro_2023-05-17_completo.pdf"
+
 
 def get_oficial_diare():
     URL = "https://doweb.rio.rj.gov.br/"
+
+    # Diretório para download do arquivo
     download_dir = (
         f"{os.path.dirname(os.path.realpath(__file__))}/oficial_diare"
     )
+
+    # Altera o diretório de download padrão
     options = Options()
     options.add_experimental_option(
         "prefs",
@@ -25,6 +31,8 @@ def get_oficial_diare():
             "safebrowsing.enabled": True,
         },
     )
+
+    # Instancia o browser
     browser = webdriver.Chrome(options=options)
     browser.get(URL)
 
@@ -33,21 +41,27 @@ def get_oficial_diare():
     sleep(5)
 
 
-def read_pdf():
-    with open(
-        "./oficial_diare/rio_de_janeiro_2023-05-17_completo.pdf", "rb"
-    ) as pdf_file:
-        pdf = PdfReader(pdf_file)
+def read_pdf(path_file):
+    # get_oficial_diare()
 
-        number_of_pages = len(pdf.pages)
+    pdf = PdfReader(path_file)
 
-        for page in range(number_of_pages):
-            content = pdf.pages[page].extract_text()
-            with open(
-                f"./excel/page_{page}.txt", "w", encoding="utf-8"
-            ) as txt_file:
-                txt_file.write(content)
+    lista = []
+
+    for index, pdf_pag in enumerate(pdf.pages):
+        content = {index + 1: pdf_pag.extract_text()}
+        lista.append(content)
+    with open("./excel/page_xablau.txt", "a", encoding="utf-8") as txt_file:
+        txt_file.write(str(lista))
 
 
-# get_oficial_diare()
-read_pdf()
+# 2 - {'atos': 1, 'do': 1, 'prefeito': 1}
+
+# AJUSTAR O NOME DO ARQUIVO AUTOMATICO
+#     tabela = pd.read_excel("excel/Diário_Oficial_Cidade_RJ.xlsx")
+#     tabela.to_excel(
+#         "excel/Diário_Oficial_Cidade_RJ_20230517.xlsx", index=False
+#     )
+
+
+read_pdf(PATH_FILE)
